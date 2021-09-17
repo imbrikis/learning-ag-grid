5a0663ce69f82312bce6dd028b88b70fb215f093
imbrikis <imbrikis@yahoo.com>
2021-09-16 19:50:45 -0600

    create blank React project w/ AG Grid dependencies

---
+++ src/components/Table.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {AgGridColumn, AgGridReact} from 'ag-grid-react';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+const Table = () => {
+  return (
+    <div>
+      <h1>AG Grid Table</h1>
+    </div>
+  )
+}
+
+export default Table
