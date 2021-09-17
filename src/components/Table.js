b45d90dbdcc9b186fedd644a6cf8de1b8c9d4ae8
imbrikis <imbrikis@yahoo.com>
2021-09-16 21:07:17 -0600

    create column definitions

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import {AgGridColumn, AgGridReact} from 'ag-grid-react';
 
+import {tabledata} from '../data/tableData'
+
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+const defaultColDef = {sortable: true, flex: 1}
+const columnTypes = {
+  main: {
+    editable: true
+  }
+}
 
 const Table = () => {
   return (
     <div>
       <h1>AG Grid Table</h1>
+      <div className="ag-theme-alpine" style={{height: 400, width: 600}}>
+        <AgGridReact rowData={tabledata} columnTypes={columnTypes} defaultColDef={defaultColDef}>
+          <AgGridColumn field="name" />
+          <AgGridColumn field="id" type="main" />
+          <AgGridColumn field="location" type="main" />
+        </AgGridReact>
+      </div>
     </div>
   )
 }
---
+++ src/data/tableData.js
@@ -0,0 +1,6 @@
+export const tabledata = [
+  {name: 'Gradius', id: 123, location: 'zero one'},
+  {name: 'Little Samson', id: 98, location: 'Europe'},
+  {name: 'Gauntlet', id: 1, location: 'Nevada'},
+  {name: "Kirby's Adventure", id: 999, location: 'Somewhere close'},
+] 
